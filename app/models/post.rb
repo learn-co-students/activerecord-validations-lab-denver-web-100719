e45a0624755030ca4f786ebcd5fb494257355a80
mezzolite <mez.charney@gmail.com>
2019-11-18 14:16:38 -0700

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ActiveRecord::Base
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+    validate :does_title_include
+
+    def does_title_include
+        errors.add(:title, "Post is invalid if not clickbait") unless
+        title.present? && title.include?("Won't Believe" || "Secret" || "Top [number]" || "Guess")
+    end
+
+
+
 end
