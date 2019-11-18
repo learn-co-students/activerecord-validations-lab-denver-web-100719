f3bf59ac73cc3b85e7ecc460811a07304ba14613
Samantha Phipps <sammi.phipps@gmail.com>
2019-11-18 14:43:08 -0700

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ActiveRecord::Base
-    validates :title, presence: true
-    validates :title, title: true
+    # validates :title, presence: true
+    # validates :title, title: true
+    validates :title, {presence: true, title: true}
     validates :content, length: { minimum: 250 }
     validates :summary, length: { maximum: 250 }
     validates :category, inclusion: { in: ["Fiction", "Non-Fiction"] }
