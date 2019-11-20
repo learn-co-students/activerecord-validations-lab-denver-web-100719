eb6239507f259bba0869805fdd82634b9f271de6
diana-mm <dmmiller85@gmail.com>
2019-11-20 13:36:53 -0700

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,21 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+    validate :is_clickbait?
+
+    def is_clickbait?
+        if self.title.present? && self.title.include?( "Won't Believe")
+            true
+        elsif self.title.present? && self.title.include?( "Secret")
+            true
+        elsif self.title.present? && self.title.include?( "Top [number]")
+            true
+        elsif self.title.present? && self.title.include?( "Guess")
+            true
+        else
+            errors.add(:title, "Not Clickbait, try new title")
+        end
+    end
 end
